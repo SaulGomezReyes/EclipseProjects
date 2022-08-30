75d7c4d83a23ead0578d51753b2013b4d355c4ba
Saul Gomez <saulol12347@gmail.com>
2022-08-30 10:49:55 -0500

    actualizacion

--- prueba/src/com/saul/abstracto/Principal.java
+++ prueba/src/com/saul/abstracto/Principal.java
@@ -5,6 +5,14 @@ public class Principal {
     public static void main(String[] args) {
         Medico medico = new Medico("Juanito","23msd3", "Licenciado", "UNAM", "Podologo","Casa");
         System.out.println("Medico -->"+medico);
-        
+        medico.trabajar();
+        medico.cobrar();
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Programador programador = new Programador("Axel", "NA", "ISC", "UTEZ", "Java", "Flask");
+        System.out.println("Programador ~~> " + programador);
+        programador.trabajar();
+        programador.cobrar();
     }
 }
